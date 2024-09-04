9d126322270193cbf40162c69c52f874077467db
abenezer <abenezermelaku3@gmail.com>
2024-09-05 00:17:36 +0300

    sgdkl

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
 
     res.send('super nigga');
 
 });
-app.get('/health', (req, res) => {
-    res.send("sever running fine");
-});
+function isValidInt32(value) {
+    const MIN_INT32 = -2147483648;
+    const MAX_INT32 = 2147483647;
 
+    return Number.isInteger(value) && value >= MIN_INT32 && value <= MAX_INT32;
+}
+app.get('/health', (req, res) => {
+    res.send("server running fine");
+});
+app.post('/sum', (req, res) => {
+    const numbers = req.body.data;
+    console.log(numbers)
+
+
+    if (!numbers.every(isValidInt32)) {
+        return res.status(400).send({ error: 'Invalid input, expected an array of int32 numbers' });
+    }
+
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
+    res.send({ result: sum });
+});
 app.listen(3000, () => {
 
     console.log('Server is running on port 3000');
